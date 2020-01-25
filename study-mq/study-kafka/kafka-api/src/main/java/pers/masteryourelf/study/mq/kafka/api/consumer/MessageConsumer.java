fbb48c53c901ac81f3a824fba92af9a6aacd0b7f
masteryourself <renzhao.ruan@foxmail.com>
2020-01-25 23:49:01 +0800

    kafka api 使用

---
+++ study-mq/study-kafka/kafka-api/src/main/java/pers/masteryourelf/study/mq/kafka/api/consumer/MessageConsumer.java
@@ -0,0 +1,45 @@
+package pers.masteryourelf.study.mq.kafka.api.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * @author : masteryourself
+ * @version : 1.0
+ * blog : https://blog.csdn.net/masteryourself
+ * Tel : 17621208646
+ * Description : 描述
+ * @date : 2020/1/25 21:15
+ */
+public class MessageConsumer {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "192.168.89.210:9092,192.168.89.220:9092,192.168.89.230:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "MessageConsumer");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                IntegerDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singleton("topic_test"));
+        while (true) {
+            ConsumerRecords<Integer, String> records = consumer.poll(1000);
+            for (ConsumerRecord<Integer, String> record : records) {
+                System.out.println("message receive: " + record.value());
+            }
+        }
+    }
+
+}
---
+++ study-mq/study-kafka/kafka-api/src/main/java/pers/masteryourelf/study/mq/kafka/api/producer/MessageProducer.java
@@ -0,0 +1,41 @@
+package pers.masteryourelf.study.mq.kafka.api.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author : masteryourself
+ * @version : 1.0
+ * blog : https://blog.csdn.net/masteryourself
+ * Tel : 17621208646
+ * Description : 描述
+ * @date : 2020/1/25 21:15
+ */
+public class MessageProducer {
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "192.168.89.210:9092,192.168.89.220:9092,192.168.89.230:9092");
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "MessageProducer");
+        properties.put(ProducerConfig.ACKS_CONFIG, "-1");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                IntegerSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        KafkaProducer<Integer, String> producer = new KafkaProducer<>(properties);
+        for (int i = 0; i < 50; i++) {
+            String message = "message:" + i;
+            System.out.println("message send: " + message);
+            producer.send(new ProducerRecord<>("topic_test", message));
+            TimeUnit.MILLISECONDS.sleep(500);
+        }
+    }
+
+}
